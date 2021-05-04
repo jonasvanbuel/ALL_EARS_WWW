e471c96c3e5cebd0be3ecf0b23ef404c05da0dab
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-05-04 19:10:07 +0100

    copy code from all_ears_coming_soon repository

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :coming_soon ]
 
   def home
+  end
+
+  def coming_soon
   end
 end
