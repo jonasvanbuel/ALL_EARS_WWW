c794672865897cf6b68639e02ad4b5d58407e832
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-05-21 23:30:22 +0100

    js test to banner scaling

--- app/javascript/packs/background_shapes_src_selection.js
+++ app/javascript/packs/background_shapes_src_selection.js
@@ -19,3 +19,18 @@ const setImageSrc = () => {
 
 window.addEventListener('load', setImageSrc);
 window.addEventListener('resize', setImageSrc);
+
+
+
+// To be externalised
+
+const scaleBanner = () => {
+  console.log("scaleBanner triggered...");
+
+  const height = window.innerHeight;
+  const banner = document.getElementById('banner');
+  banner.style.height = `${height}px`
+}
+
+window.addEventListener('load', scaleBanner);
+window.addEventListener('resize', scaleBanner);
