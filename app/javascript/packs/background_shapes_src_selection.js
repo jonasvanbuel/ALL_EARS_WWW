58a2c6424956a0675b8b4e785e2d64090db91b7b
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-05-21 18:08:31 +0100

    coming-soon page refresh

---
+++ app/javascript/packs/background_shapes_src_selection.js
@@ -0,0 +1,21 @@
+const setImageSrc = () => {
+  console.log("Background_shapes_src_selection pack triggered...");
+
+  const el = document.getElementById('background-shapes-image');
+  const width = window.innerWidth;
+  const mobileBreakpoint = 768;
+  let src = null
+
+  // debugger
+
+  if (width <= mobileBreakpoint) {
+    src = el.dataset.mobileSrc;
+  } else {
+    src = el.dataset.desktopSrc;
+  }
+
+  el.style.backgroundImage = `url(${src})`;
+}
+
+window.addEventListener('load', setImageSrc);
+window.addEventListener('resize', setImageSrc);
