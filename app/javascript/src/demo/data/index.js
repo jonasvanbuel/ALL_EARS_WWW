c877b138b3b267e28b3b811e6d4e0b7ee9bf5618
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2022-06-19 10:56:34 +0200

    add Virginia Overton data

--- app/javascript/src/demo/data/index.js
+++ app/javascript/src/demo/data/index.js
@@ -1,10 +1,12 @@
 // Import and add new json exhibition files below...
 import { bruceNaumanYu82d } from './bruce-nauman-yu82d';
 import { claudiaSchifferM57tu } from './claudia-schiffer-m57tu';
+import { virginiaOvertonN6w81 } from './virginia-overton-n6w81';
 
 const exhibitions = {
   bruceNaumanYu82d,
-  claudiaSchifferM57tu
+  claudiaSchifferM57tu,
+  virginiaOvertonN6w81
 }
 
 export default exhibitions;
