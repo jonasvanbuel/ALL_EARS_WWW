9b9bef697a6844ff6745312b0c4883068252d855
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2022-07-04 10:21:05 +0200

    data upload

--- app/javascript/src/demo/data/virginia-overton-n6w81.js
+++ app/javascript/src/demo/data/virginia-overton-n6w81.js
@@ -16,15 +16,15 @@ export const virginiaOvertonN6w81 = {
       id: 1,
       chapterNumber: 1,
       roomNumber: 1,
-      title: 'Untitled, 2015',
+      title: 'Untitled (Rumination), 2022',
       imgSources: {
-        mobile: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1655628840/2022-all-ears-virginia-overton/7_Untitled_2015_zdtx9t.jpg',
-        desktop: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1655628840/2022-all-ears-virginia-overton/7_Untitled_2015_zdtx9t.jpg'
+        mobile: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921271/2022-all-ears-virginia-overton/6_Untitled_rumination_2022_LR_skncr4.jpg',
+        desktop: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921271/2022-all-ears-virginia-overton/6_Untitled_rumination_2022_LR_skncr4.jpg'
       },
       audioSources: [
         {
           type: 'mpeg',
-          src: 'https://res.cloudinary.com/deo4sjfc8/video/upload/v1655628840/2022-all-ears-virginia-overton/7_Untitled_2015_oxmfjk.mp3'
+          src: 'https://res.cloudinary.com/deo4sjfc8/video/upload/v1656921275/2022-all-ears-virginia-overton/6_Untitled_rumination_2022_xbduvy.mp3'
         }
       ]
     },
@@ -32,15 +32,95 @@ export const virginiaOvertonN6w81 = {
       id: 2,
       chapterNumber: 2,
       roomNumber: 2,
-      title: 'Untitled (Arc), 2022',
+      title: 'Untitled, 2015',
       imgSources: {
-        mobile: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1655628843/2022-all-ears-virginia-overton/14_Untitled_Arc_2022_dk0nut.jpg',
-        desktop: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1655628843/2022-all-ears-virginia-overton/14_Untitled_Arc_2022_dk0nut.jpg'
+        mobile: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921277/2022-all-ears-virginia-overton/7_Untitled_2015_LR_wpiaje.jpg',
+        desktop: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921277/2022-all-ears-virginia-overton/7_Untitled_2015_LR_wpiaje.jpg'
       },
       audioSources: [
         {
           type: 'mpeg',
-          src: 'https://res.cloudinary.com/deo4sjfc8/video/upload/v1655628837/2022-all-ears-virginia-overton/14_Untitled_Arc_2022_mrayto.mp3'
+          src: 'https://res.cloudinary.com/deo4sjfc8/video/upload/v1656921280/2022-all-ears-virginia-overton/7_Untitled_2015_lmfnv0.mp3'
+        }
+      ]
+    },
+    {
+      id: 3,
+      chapterNumber: 3,
+      roomNumber: 3,
+      title: 'Untitled (Stance), 2015',
+      imgSources: {
+        mobile: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921282/2022-all-ears-virginia-overton/12_Untitled_stance_2015_LR_l8ezyk.jpg',
+        desktop: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921282/2022-all-ears-virginia-overton/12_Untitled_stance_2015_LR_l8ezyk.jpg'
+      },
+      audioSources: [
+        {
+          type: 'mpeg',
+          src: 'https://res.cloudinary.com/deo4sjfc8/video/upload/v1656921286/2022-all-ears-virginia-overton/12_Untitled_stance_2015_y2gqnf.mp3'
+        }
+      ]
+    },
+    {
+      id: 4,
+      chapterNumber: 4,
+      roomNumber: 4,
+      title: 'Untitled (Arco sculpture), 2021',
+      imgSources: {
+        mobile: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921288/2022-all-ears-virginia-overton/13_Untitled_Arco_sculpture_2021_LR_xqkcav.jpg',
+        desktop: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921288/2022-all-ears-virginia-overton/13_Untitled_Arco_sculpture_2021_LR_xqkcav.jpg'
+      },
+      audioSources: [
+        {
+          type: 'mpeg',
+          src: 'https://res.cloudinary.com/deo4sjfc8/video/upload/v1656921291/2022-all-ears-virginia-overton/13_Untitled_Arco_sculpture_2021_aim0ql.mp3'
+        }
+      ]
+    },
+    {
+      id: 5,
+      chapterNumber: 5,
+      roomNumber: 5,
+      title: 'Untitled (Arc), 2022',
+      imgSources: {
+        mobile: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921294/2022-all-ears-virginia-overton/14_Untitled_Arc_2022_LR_ebh3as.jpg',
+        desktop: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921294/2022-all-ears-virginia-overton/14_Untitled_Arc_2022_LR_ebh3as.jpg'
+      },
+      audioSources: [
+        {
+          type: 'mpeg',
+          src: 'https://res.cloudinary.com/deo4sjfc8/video/upload/v1656921297/2022-all-ears-virginia-overton/14_Untitled_Arc_2022_lv9cwv.mp3'
+        }
+      ]
+    },
+    {
+      id: 6,
+      chapterNumber: 6,
+      roomNumber: 6,
+      title: 'Untitled (Chime for Caro), 2022',
+      imgSources: {
+        mobile: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921299/2022-all-ears-virginia-overton/17_Untitled_chime_for_Caro_2022_LR_h6uyot.jpg',
+        desktop: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921299/2022-all-ears-virginia-overton/17_Untitled_chime_for_Caro_2022_LR_h6uyot.jpg'
+      },
+      audioSources: [
+        {
+          type: 'mpeg',
+          src: 'https://res.cloudinary.com/deo4sjfc8/video/upload/v1656921305/2022-all-ears-virginia-overton/17_Untitled_chime_for_Caro_2022_pau1xo.mp3'
+        }
+      ]
+    },
+    {
+      id: 7,
+      chapterNumber: 7,
+      roomNumber: 7,
+      title: 'Untitled (Sculpture on table), 2022',
+      imgSources: {
+        mobile: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921307/2022-all-ears-virginia-overton/18_Untitled_sculpture_on_table_2022_LR_cbggpl.jpg',
+        desktop: 'https://res.cloudinary.com/deo4sjfc8/image/upload/v1656921307/2022-all-ears-virginia-overton/18_Untitled_sculpture_on_table_2022_LR_cbggpl.jpg'
+      },
+      audioSources: [
+        {
+          type: 'mpeg',
+          src: 'https://res.cloudinary.com/deo4sjfc8/video/upload/v1656921310/2022-all-ears-virginia-overton/18_Untitled_sculpture_on_table_2022_nbbcqt.mp3'
         }
       ]
     }
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#coming_soon'
   get 'demo', to: 'pages#demo'
+  # get 'virginia_overton_2022', to: 'pages#exhibition'
 end
